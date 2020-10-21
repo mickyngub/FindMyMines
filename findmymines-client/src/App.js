ea38ed7256f131d4225e15984ba5724fbb41daea
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-10-21 21:03:08 +0700

    before change

--- findmymines-client/src/App.js
+++ findmymines-client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
-import Grid from "./Components/Grids";
+import Grids from "./Components/Grids";
+
+import io from "socket.io-client";
+
 function App() {
+  const socket = io("https://localhost:8000");
+
+  let onClickHandler = () => {
+    socket.emit("chat message", "I hope this work");
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{ marginTop: "5vh" }}>
       Hey welcome to FindMyMines by Micky-Pinn-Boss
-      <Grid></Grid>
+      <Grids></Grids>
+      <ul id="messages"></ul>
+      <div onClick={onClickHandler}>Send</div>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+    console.log("chat message" + msg);
   });
 });
 
